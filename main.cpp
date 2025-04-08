58720eb9457953ab38c3a5f45ae425b0cb6012e4
FrancoAlminagorda <alminagordafranco0209@gmail.com>
2025-04-07 21:45:06 -0500

    Comentario añadido

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream> // cout
 
+using namespace std;
+// asi ya no es necesario escribir el std::  
+
 int main(){
-    std::cout << "Hola mundo desde Fund Prog 2025-II" << std::endl;
+cout << "Hola mundo desde Fund Prog 2025-II" <<endl;
     return 0;
-}
+} 
